4307f2c0aed372e4888f2851b6861c2021c0a9b2
Yohanes Handoyo <yohanes7286@gmail.com>
2024-05-23 00:45:19 +0700

    Setup passport

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\FurnitureController;
 
-
 Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+})->middleware('auth:api');
 
 Route::apiResource('furnitures', FurnitureController::class);
-
